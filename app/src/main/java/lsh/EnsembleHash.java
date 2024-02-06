00fe00f080f562ed16d75a5521de4cf429dd77de
Malte Johnsen <mhej@itu.dk>
2024-02-06 10:37:28 +0100

    Add buildIndex method - modify to float datatype

--- app/src/main/java/lsh/EnsembleHash.java
+++ app/src/main/java/lsh/EnsembleHash.java
@@ -4,28 +4,60 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Random;
 
-import ch.systemsx.cisd.hdf5.io.*;
-
-
-//ADD COMMENT HERE
+import org.apache.commons.io.output.StringBuilderWriter;
 
 public class EnsembleHash {
 
     private List<HashFunction> hashFunctions;
     private Map<String, List<Integer>> hashIndex;
+    private float[][] corpusMatrix;
     
-    public EnsembleHash(int k, int d, double r) {
+    public EnsembleHash(int k, float[][] corpusMatrix, double r) {
 
+        hashIndex = new HashMap<>();
+        this.corpusMatrix = corpusMatrix;
+    
         hashFunctions = new LinkedList<>();
         for (int i = 0; i < k; i++) {
-            hashFunctions.add(new HashFunction(d, r));
-        }   
+            hashFunctions.add(new HashFunction(corpusMatrix[0].length, r));
+        }
 
     }
 
-    public int[] query(double[] qVec) {
+    public void buildIndex() {
+        for (int cIndex = 0; cIndex < corpusMatrix.length; cIndex++) {
+            float[] cVec = corpusMatrix[cIndex];
+
+           
+            String label = getLabel(cVec);
+
+            // Add index to partition
+            List<Integer> partition = hashIndex.get(label);
+            if (partition == null) {
+                partition = new LinkedList<>();
+                partition.add(cIndex);
+                hashIndex.put(label, partition);
+                continue;
+            }
+            partition.add(cIndex);
+        }
+        System.out.println(hashIndex.toString());
+    }
+
+    private String getLabel(float[] vec) {
+
+        StringBuilder strBuild = new StringBuilder();
+        for (HashFunction hashFunction : hashFunctions) {
+            strBuild.append(hashFunction.hash(vec));
+        }
+        
+        return strBuild.toString();
+    }
+
+    public int[] query(float[] qVec) {
     
         // TO IMPLEMENT
         int[] returnValue = {1,2,3};
@@ -34,7 +66,7 @@ public class EnsembleHash {
 
     class HashFunction {
 
-        private double[] aVec;
+        private float[] aVec;
         private double b;
         private double r;
 
@@ -44,12 +76,12 @@ public class EnsembleHash {
             // b is drawn from uniform distribution [0,r]
             Random randomGen = new Random();
 
-            b = randomGen.nextDouble() * r;
+            b = randomGen.nextFloat() * r;
 
             // random d-dimensional vector where each component is drawn from N(0,1)
-            aVec = new double[d];
+            aVec = new float[d];
             for (int i = 0; i < d; i++) {
-                aVec[i] = randomGen.nextGaussian();
+                aVec[i] = (float) randomGen.nextGaussian();
 
             }
 
@@ -58,10 +90,10 @@ public class EnsembleHash {
         // Implement constructor with seed value for testing? Will generate same random
         // vector each time
 
-        public int hash(double[] xVec) {
+        public int hash(float[] xVec) {
 
             // Calculate a.x
-            double dotProd = Utils.dot(xVec, aVec);
+            float dotProd = Utils.dot(xVec, aVec);
 
             // Calculate the hash-value
             return (int) Math.floor((dotProd + b) / r);
--- app/src/main/java/lsh/Utils.java
+++ app/src/main/java/lsh/Utils.java
@@ -3,13 +3,13 @@ package lsh;
 public class Utils {
     
 
-    public static double dot(double[] aVec, double[] bVec){
+    public static float dot(float[] aVec, float[] bVec){
 
         if (aVec.length != bVec.length) {
             throw new IllegalArgumentException("Vectors of unequal length passed");
         }
 
-        double dotProduct = 0.0;
+        float dotProduct = 0.0f;
         
         for (int i = 0; i < aVec.length; i++) {
             dotProduct += aVec[i] * bVec[i];
@@ -18,11 +18,11 @@ public class Utils {
         return dotProduct;
     }
 
-    public static double euclideanDistance(double[] aVec, double[] bVec) {
+    public static float euclideanDistance(float[] aVec, float[] bVec) {
 
         // To be implemented
 
-        return -1.0;
+        return -1.0f;
     }
 
     //NEW METHOD HERE
